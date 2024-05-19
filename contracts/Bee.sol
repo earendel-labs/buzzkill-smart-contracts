6e998530819277900086f1925ca872bf5c934638
DustinSun888 <dustinsun888@gmail.com>
2024-05-19 15:45:28 +0700

    temp

--- contracts/Bee.sol
+++ contracts/Bee.sol
@@ -1,34 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Bee {
-    uint public unlockTime;
-    address payable public owner;
+contract Bee is ERC721 {
+    uint256 private _beeCount;
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    enum BeeType {
+        Queen,
+        Worker
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    struct BeeData {
+        BeeType beeType;
+        string name;
+        uint256 age;
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    mapping(uint256 => BeeData) private _bees;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    constructor() ERC721("Bee", "BEE") {}
 
-        owner.transfer(address(this).balance);
+    function mintBee(
+        BeeType beeType,
+        string memory name,
+        uint256 age
+    ) external {
+        uint256 tokenId = _beeCount;
+        _safeMint(msg.sender, tokenId);
+        _bees[tokenId] = BeeData(beeType, name, age);
+        _beeCount++;
+    }
+
+    function getBeeData(
+        uint256 tokenId
+    ) external view returns (BeeData memory) {
+        return _bees[tokenId];
     }
 }
